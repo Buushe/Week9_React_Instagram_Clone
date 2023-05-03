cd99f9e678555dbe25a6ea5465460e829b56baf3
Abdirahman Mohamed (Buushe) <79105703+Buushe@users.noreply.github.com>
2023-05-04 00:40:28 +0600

    i have done

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -6,11 +6,14 @@ const Comments = props => {
   // 🔥 Make sure the parent of Comments is passing the right props!
   // Hubi in component-ga ka sareysa midaan ay props sax ah soo direyso.
   const { comments } = props;
-
+  console.log('Comments',props.comments.text);
   return (
     <div>
       {/* map through the comments prop and render a Comment for every piece of data */}
       {/* Dulmar prop-ka 'comments', kadib soo celi 'Comment' component, adigoo waxa ay 'Comment' u baahantahay u diraayo. */}
+      {comments.map(singleComment =>(
+     <Comment comment = {singleComment } />
+     ))}
     </div>
   );
 };
--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -6,16 +6,14 @@ import { faHeart, faCircle, faCompass } from '@fortawesome/free-regular-svg-icon
 import "./SearchBar.css";
 
 const SearchBar = (props) => {
+  <p>search {props.SearchBar}</p>
   return (
     <div className="search-bar-wrapper">
       <div className="social">
         <FontAwesomeIcon icon={faInstagram} />
       </div>
       <form className="search-form">
-        <input
-          type="text"
-          placeholder="Search"
-        />
+      <input type="text" placeholder="Search"/>
       </form>
       <div className="social-wrapper">
         <div className="social">
